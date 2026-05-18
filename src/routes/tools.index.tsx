432a8a0c2e0c58c791338b7f611d7a6a61195841
Fortune Gumiliza <fortune@mac.home>
2026-05-18 22:40:56 +0200

    feat: alle vertalingen en diep-etste games achtergrond live zetten

--- src/routes/tools.index.tsx
+++ src/routes/tools.index.tsx
@@ -377,7 +377,8 @@ function ArcadeTextureSVG() {
     <svg
       aria-hidden
       style={{
-        position: "absolute", inset: 0,
+        position: "absolute",
+        top: 0, right: 0, bottom: 0, left: 0,
         width: "100%", height: "100%",
         zIndex: 0, pointerEvents: "none",
       }}
